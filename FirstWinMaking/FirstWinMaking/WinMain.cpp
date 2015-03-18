6c7d9d830ad12f5c20f694bf15109e711c865724
abiles <pgalpga@hanmail.net>
2015-03-18 19:16:45 +0900

    circle to box 까지

--- FirstWinMaking/FirstWinMaking/WinMain.cpp
+++ FirstWinMaking/FirstWinMaking/WinMain.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include <time.h>
 #include "resource1.h"
+#include <math.h>
 
 #define szWindowClass	TEXT("First")
 #define szTitle			TEXT("First App")
@@ -9,14 +10,31 @@
 #define MAXHORIZONTAL 800
 #define MAXVERTICAL 600
 
-// 전역으로 메모리 DC가 이곳에 bit맵을 그려준다. 
-HBITMAP gBitMap;
+struct Coordinate
+{
+	float posX = 0;
+	float posY = 0;
+};
 
+
+float mousePosX = 0;
+float mousePosY = 0;
 
 int createWNDClass(OUT WNDCLASSEX& wcex, IN HINSTANCE hInstance);
 int initHWND(OUT HWND& hWnd, IN HINSTANCE hInstance);
 int initAppTitle(IN HWND hWnd, IN HINSTANCE hInstance);
 int initBackground(IN HDC hdc, IN HWND hWnd);
+int createLinearAndPoint(IN HDC hdc);
+int collisionBoxToBox(IN HDC hdc);
+bool boxToBoxCollisonCheck(Coordinate pos, int startX, int startY, int endX, int endY);
+bool circleToCircleCollisionCheck(int FirstRadius, Coordinate firstCenterPos,
+	int SecondRadius, Coordinate secondCenterPos);
+int collisionCircleToCircle(IN HDC hdc);
+int collisionBoxToCircle(IN HDC hdc);
+int collisionBoxToCircleNew(IN HDC hdc);
+bool choiceBoxCheckOrCircleCheck(Coordinate boxStartPos, Coordinate boxEndPos,
+	Coordinate circleCenterPos);
+
 
 void makingFigure(HDC hdc, int downPosX, int downPosY);
 void makingFollowRect(HDC hdc, int x, int y);
@@ -49,11 +67,31 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 	ShowWindow(hWnd, nCmdShow);
 
 	MSG	msg;
-	while (GetMessage(&msg, NULL, 0, 0))
+	
+	HDC hdc = GetDC(hWnd);
+
+	// 게임용 메세지 처리 루프 
+	while (1)
 	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (WM_QUIT == msg.message)
+				break;
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			//createLinearAndPoint(hdc);
+			//collisionBoxToBox(hdc);
+			//collisionCircleToCircle(hdc);
+			//collisionBoxToCircle(hdc);
+			collisionBoxToCircleNew(hdc);
+		}
 	}
+	
+	ReleaseDC(hWnd, hdc);
+
 
 	return (int)msg.wParam;
 }
@@ -86,7 +124,7 @@ LRESULT CALLBACK WndProc(HWND hWnd
 	}
 	case WM_PAINT:
 	{
-		hdc = BeginPaint(hWnd, &ps);
+	/*	hdc = BeginPaint(hWnd, &ps);
 		HDC memoryDC = CreateCompatibleDC(hdc);
 		HBITMAP memoryBitmap = CreateCompatibleBitmap(hdc, MAXHORIZONTAL, MAXVERTICAL);
 		HBITMAP memoryOldBitmap = (HBITMAP)SelectObject(memoryDC, memoryBitmap);
@@ -106,10 +144,10 @@ LRESULT CALLBACK WndProc(HWND hWnd
 
 		SelectObject(imgDC, imgBitmap);
 		DeleteObject(imgBitmap);
-		DeleteObject(imgDC);
+		DeleteObject(imgDC);*/
 
 		
-		EndPaint(hWnd, &ps);
+		//EndPaint(hWnd, &ps);
 	}
 		break;
 	case WM_LBUTTONDOWN:
@@ -117,10 +155,10 @@ LRESULT CALLBACK WndProc(HWND hWnd
 		int downPosX = LOWORD(lParam);
 		int downPosY = HIWORD(lParam);
 		HDC hdc = GetDC(hWnd);
-		makingFigure(hdc, downPosX, downPosY);
+		//makingFigure(hdc, downPosX, downPosY);
 		ReleaseDC(hWnd, hdc);
 
-		InvalidateRect(hWnd, NULL, false);
+		//InvalidateRect(hWnd, NULL, false);
 
 	}
 	break;
@@ -136,6 +174,8 @@ LRESULT CALLBACK WndProc(HWND hWnd
 		break;
 	case WM_MOUSEMOVE:
 		{
+						 mousePosX = LOWORD(lParam);
+						 mousePosY = HIWORD(lParam);
 			/*int mouseX = LOWORD(lParam);
 			int mouseY = HIWORD(lParam);
 			
@@ -311,4 +351,388 @@ int initBackground(IN HDC hdc, IN HWND hWnd)
 	DeleteObject(imgDC);
 
 	return 1;
+}
+
+int createLinearAndPoint(IN HDC hdc)
+{
+	//a먼저 선을 그리면
+
+	HDC memoryDC = CreateCompatibleDC(hdc);
+	HBITMAP memoryBitmap = CreateCompatibleBitmap(hdc, MAXHORIZONTAL, MAXVERTICAL);
+	SelectObject(memoryDC, memoryBitmap);
+
+	Rectangle(memoryDC, 0, 0, MAXHORIZONTAL, MAXVERTICAL);
+
+	TCHAR xBuffer[200] = { 0, };
+	TCHAR yBuffer[200] = { 0, };
+	TCHAR dBuffer[200] = { 0, };
+
+	wsprintf(xBuffer, L"X : %d", mousePosX);
+	wsprintf(yBuffer, L"Y : %d", mousePosY);
+
+	TextOut(memoryDC, 700, 20, xBuffer, wcslen(xBuffer));
+	TextOut(memoryDC, 700, 40, yBuffer, wcslen(yBuffer));
+
+	int linearStartPosX = 300;
+	int linearStartPosY = 0;
+	int linearEndPosX = 600;
+	int linearEndPosY = 600;
+
+	MoveToEx(memoryDC, linearStartPosX, linearStartPosY, NULL);
+	LineTo(memoryDC, linearEndPosX, linearEndPosY);
+
+	int a = 2;
+	int b = -1;
+	int c = -600;
+
+	float denominator = (a * a) + (-b * -b);
+	int distance = (a * mousePosX + b * mousePosY + c) / sqrt(denominator);
+
+	wsprintf(dBuffer, L"D : %d", distance);
+	TextOut(memoryDC, 700, 60, dBuffer, wcslen(dBuffer));
+
+	int meetX = ((mousePosX) + (2 * mousePosY) + 2 * (-c)) / 5;
+	int meetY = 2 * meetX + c;
+
+	MoveToEx(memoryDC, mousePosX, mousePosY, NULL);
+	LineTo(memoryDC, meetX, meetY);
+	
+	BitBlt(hdc, 0, 0, MAXHORIZONTAL, MAXVERTICAL, memoryDC, 0, 0, SRCCOPY);
+
+	DeleteObject(memoryBitmap);
+	DeleteDC(memoryDC);
+
+
+	return 1;
+}
+
+int collisionBoxToBox(IN HDC hdc)
+{
+	HDC memoryDC = CreateCompatibleDC(hdc);
+	HBITMAP memoryBitmap = CreateCompatibleBitmap(hdc, MAXHORIZONTAL, MAXVERTICAL);
+	SelectObject(memoryDC, memoryBitmap);
+
+	Rectangle(memoryDC, 0, 0,MAXHORIZONTAL, MAXVERTICAL);
+
+	//create fixedBox
+	HBRUSH makingBrush = CreateSolidBrush(RGB(250, 0, 0));
+	HBRUSH defaultBrush = (HBRUSH)SelectObject(memoryDC, makingBrush);
+	Rectangle(memoryDC, 250, 250, 500, 500);
+	
+	const int rectPoitSize = 4;
+	Coordinate rectCoordi[rectPoitSize];
+	rectCoordi[0].posX = mousePosX;
+	rectCoordi[0].posY = mousePosY;
+	rectCoordi[1].posX = mousePosX;
+	rectCoordi[1].posY = mousePosY + 50;
+	rectCoordi[2].posX = mousePosX + 50;
+	rectCoordi[2].posY = mousePosY;
+	rectCoordi[3].posX = mousePosX + 50;
+	rectCoordi[3].posY = mousePosY + 50;
+
+	//create mouse FllowBox
+	makingBrush = CreateSolidBrush(RGB(0, 250, 0));
+	SelectObject(memoryDC, makingBrush);
+	
+	Rectangle(memoryDC, mousePosX, mousePosY, mousePosX + 50, mousePosY + 50);
+
+	//collision check
+	bool collisionCheck = false;
+	for (int i = 0; i < rectPoitSize; ++i)
+	{
+		if (boxToBoxCollisonCheck(rectCoordi[i], 250, 250, 500, 500))
+		{
+			collisionCheck = true;
+		}
+	}
+
+	if (collisionCheck)
+	{
+		makingBrush = CreateSolidBrush(RGB(0, 0, 250));
+		SelectObject(memoryDC, makingBrush);
+		Rectangle(memoryDC, mousePosX, mousePosY, mousePosX + 50, mousePosY + 50);
+	}
+	
+	BitBlt(hdc, 0, 0, MAXHORIZONTAL, MAXVERTICAL, memoryDC, 0, 0, SRCCOPY);
+
+	DeleteObject(makingBrush);
+	DeleteObject(memoryBitmap);
+	DeleteDC(memoryDC);
+
+	return 1;
+}
+
+int collisionCircleToCircle(IN HDC hdc)
+{
+
+	HDC memoryDC = CreateCompatibleDC(hdc);
+	HBITMAP memoryBitmap = CreateCompatibleBitmap(hdc, MAXHORIZONTAL, MAXVERTICAL);
+	SelectObject(memoryDC, memoryBitmap);
+	
+	Rectangle(memoryDC, 0, 0, MAXHORIZONTAL, MAXVERTICAL);
+
+	//create fixedCircle
+	HBRUSH makingBrush = CreateSolidBrush(RGB(250, 0, 0));
+	HBRUSH defaultBrush = (HBRUSH)SelectObject(memoryDC, makingBrush);
+
+	float fixedCircleCenterX = 300;
+	float fixedCircleCenterY = 300;
+	Ellipse(memoryDC, fixedCircleCenterX - 100, fixedCircleCenterY - 100,
+		fixedCircleCenterX + 100, fixedCircleCenterY + 100);
+	int fixedCircleRadius = 100;
+	Coordinate fixedCirclePos;
+	fixedCirclePos.posX = fixedCircleCenterX;
+	fixedCirclePos.posY = fixedCircleCenterY;
+
+
+	//crate mouseCircle
+	makingBrush = CreateSolidBrush(RGB(0, 250, 0));
+	SelectObject(memoryDC, makingBrush);
+	Ellipse(memoryDC, mousePosX - 50, mousePosY - 50, mousePosX + 50, mousePosY + 50);
+	// 마우스 포즈가 원점
+	int mouseCircleRadius = 50;
+
+	int distance = sqrt((pow(fixedCircleCenterX - mousePosX, 2) + pow(fixedCircleCenterY - mousePosY, 2)));
+
+	bool checkCollision = false;
+
+	Coordinate mousePos;
+	mousePos.posX = mousePosX;
+	mousePos.posY = mousePosY;
+	if (circleToCircleCollisionCheck(fixedCircleRadius,fixedCirclePos, mouseCircleRadius, mousePos ))
+	{
+		checkCollision = true;
+	}
+
+	if (checkCollision)
+	{
+		makingBrush = CreateSolidBrush(RGB(0,0 , 250));
+		SelectObject(memoryDC, makingBrush);
+		Ellipse(memoryDC, mousePosX - 50, mousePosY - 50, mousePosX + 50, mousePosY + 50);
+	}
+
+	BitBlt(hdc, 0, 0, MAXHORIZONTAL, MAXVERTICAL, memoryDC, 0, 0, SRCCOPY);
+
+	DeleteObject(makingBrush);
+	DeleteObject(memoryBitmap);
+	DeleteDC(memoryDC);
+
+	return 1;
+}
+
+int collisionBoxToCircle(IN HDC hdc)
+{
+	HDC memoryDC = CreateCompatibleDC(hdc);
+	HBITMAP memoryBitmap = CreateCompatibleBitmap(hdc, MAXHORIZONTAL, MAXVERTICAL);
+	SelectObject(memoryDC, memoryBitmap);
+
+	Rectangle(memoryDC, 0, 0, MAXHORIZONTAL, MAXVERTICAL);
+
+	//create fixedCircle
+	HBRUSH makingBrush = CreateSolidBrush(RGB(250, 0, 0));
+	HBRUSH defaultBrush = (HBRUSH)SelectObject(memoryDC, makingBrush);
+
+	float fixedCircleCenterX = 300;
+	float fixedCircleCenterY = 300;
+
+	Ellipse(memoryDC, fixedCircleCenterX - 100, fixedCircleCenterY - 100,
+		fixedCircleCenterX + 100, fixedCircleCenterY + 100);
+	int fixedCircleRadius = 100;
+
+
+	//create mouse FllowBox
+	makingBrush = CreateSolidBrush(RGB(0, 250, 0));
+	SelectObject(memoryDC, makingBrush);
+
+	int rectSize = 50;
+	Rectangle(memoryDC, mousePosX, mousePosY, mousePosX + rectSize, mousePosY + rectSize);
+
+	const int coordiSize = 12;
+	Coordinate boxCoordi[coordiSize];
+	boxCoordi[0].posX = mousePosX;
+	boxCoordi[0].posY = mousePosY;
+	boxCoordi[1].posX = mousePosX + rectSize / 3;
+	boxCoordi[1].posY = mousePosY;
+	boxCoordi[2].posX = mousePosX + (rectSize * 2) / 3;
+	boxCoordi[2].posY = mousePosY;
+	boxCoordi[3].posX = mousePosX + rectSize;
+	boxCoordi[3].posY = mousePosY;
+	boxCoordi[4].posX = mousePosX + rectSize;
+	boxCoordi[4].posY = mousePosY + rectSize /3;
+	boxCoordi[5].posX = mousePosX + rectSize;
+	boxCoordi[5].posY = mousePosY + (rectSize * 2) / 3;
+	boxCoordi[6].posX = mousePosX + rectSize;
+	boxCoordi[6].posY = mousePosY + rectSize;
+	boxCoordi[7].posX = mousePosX + (rectSize * 2) / 3;
+	boxCoordi[7].posY = mousePosY + rectSize;
+	boxCoordi[8].posX = mousePosX + rectSize / 3;
+	boxCoordi[8].posY = mousePosY + rectSize;
+	boxCoordi[9].posX = mousePosX;
+	boxCoordi[9].posY = mousePosY + rectSize;
+	boxCoordi[10].posX = mousePosX;
+	boxCoordi[10].posY = mousePosY + (rectSize*2) /3;
+	boxCoordi[11].posX = mousePosX;
+	boxCoordi[11].posY = mousePosY + rectSize/3;
+
+	bool collisonCheck = false;
+	for (int i = 0; i < coordiSize; ++i)
+	{
+		int distance = sqrt(pow(boxCoordi[i].posX - fixedCircleCenterX, 2) +
+			pow(boxCoordi[i].posY - fixedCircleCenterY, 2));
+		if (distance <= fixedCircleRadius)
+		{
+			collisonCheck = true;
+			break;
+		}
+	}
+
+	if (collisonCheck)
+	{
+		makingBrush = CreateSolidBrush(RGB(0, 0, 250));
+		SelectObject(memoryDC, makingBrush);
+
+		Rectangle(memoryDC, mousePosX, mousePosY, mousePosX + rectSize, mousePosY + rectSize);
+	}
+
+	BitBlt(hdc, 0, 0, MAXHORIZONTAL, MAXVERTICAL, memoryDC, 0, 0, SRCCOPY);
+
+	DeleteObject(makingBrush);
+	DeleteObject(memoryBitmap);
+	DeleteDC(memoryDC);
+
+	return 1;
+}
+
+int collisionBoxToCircleNew(IN HDC hdc)
+{
+
+	HDC memoryDC = CreateCompatibleDC(hdc);
+	HBITMAP memoryBitmap = CreateCompatibleBitmap(hdc, MAXHORIZONTAL, MAXVERTICAL);
+	SelectObject(memoryDC, memoryBitmap);
+
+	Rectangle(memoryDC, 0, 0, MAXHORIZONTAL, MAXVERTICAL);
+
+	//create fixedCircle
+	HBRUSH makingBrush = CreateSolidBrush(RGB(250, 0, 0));
+	HBRUSH defaultBrush = (HBRUSH)SelectObject(memoryDC, makingBrush);
+
+	float fixedCircleCenterX = 300;
+	float fixedCircleCenterY = 300;
+
+	Ellipse(memoryDC, fixedCircleCenterX - 100, fixedCircleCenterY - 100,
+		fixedCircleCenterX + 100, fixedCircleCenterY + 100);
+	int fixedCircleRadius = 100;
+
+
+	//create mouse FllowBox
+	makingBrush = CreateSolidBrush(RGB(0, 250, 0));
+	SelectObject(memoryDC, makingBrush);
+
+	int rectSize = 50;
+	Rectangle(memoryDC, mousePosX, mousePosY, mousePosX + rectSize, mousePosY + rectSize);
+
+	Coordinate rectCoordinate[4];
+	rectCoordinate[0].posX = mousePosX;
+	rectCoordinate[0].posY = mousePosY;
+	rectCoordinate[1].posX = mousePosX + rectSize;
+	rectCoordinate[1].posY = mousePosY;
+	rectCoordinate[2].posX = mousePosX;
+	rectCoordinate[2].posY = mousePosY + rectSize;
+	rectCoordinate[3].posX = mousePosX + rectSize;
+	rectCoordinate[3].posY = mousePosY + rectSize;
+
+	Coordinate boxStartPos;
+	boxStartPos.posX = mousePosX;
+	boxStartPos.posY = mousePosY;
+
+	Coordinate boxEndPos;
+	boxEndPos.posX = mousePosX + rectSize;
+	boxEndPos.posY = mousePosY + rectSize;
+
+	Coordinate circleCenterPos;
+	circleCenterPos.posX = fixedCircleCenterX;
+	circleCenterPos.posY = fixedCircleCenterY;
+
+	bool isBoxCheck = choiceBoxCheckOrCircleCheck(boxStartPos, boxEndPos, circleCenterPos);
+	
+	bool isCollision = false;
+	if (isBoxCheck)
+	{
+		for (int i = 0; i < 4; ++i)
+		{
+			isCollision = boxToBoxCollisonCheck(rectCoordinate[i], fixedCircleCenterX - fixedCircleRadius,
+				fixedCircleCenterY - fixedCircleRadius, fixedCircleCenterX + fixedCircleRadius, fixedCircleCenterY - fixedCircleRadius);
+			if (isCollision)
+				break;
+		}
+	}
+	else
+	{
+		for (int i = 0; i < 4; ++i)
+		{
+			isCollision = circleToCircleCollisionCheck(fixedCircleRadius, circleCenterPos, 0, rectCoordinate[i]);
+			if (isCollision)
+				break;
+		}
+	}
+
+	if (isCollision)
+	{
+		makingBrush = CreateSolidBrush(RGB(0, 0, 250));
+		SelectObject(memoryDC, makingBrush);
+		Rectangle(memoryDC, mousePosX, mousePosY, mousePosX + rectSize, mousePosY + rectSize);
+	}
+
+	BitBlt(hdc, 0, 0, MAXHORIZONTAL, MAXVERTICAL, memoryDC, 0, 0, SRCCOPY);
+
+	DeleteObject(makingBrush);
+	DeleteObject(memoryBitmap);
+	DeleteDC(memoryDC);
+
+	return 1;
+}
+
+bool boxToBoxCollisonCheck(Coordinate eachPos, int startX, int startY, int endX, int endY)
+{
+	if (eachPos.posX < startX)
+		return false;
+
+	if (eachPos.posX > endX)
+		return false;
+
+	if (eachPos.posY < startY)
+		return false;
+
+	if (eachPos.posY > endY)
+		return false;
+
+	return true;
+
+}
+
+bool circleToCircleCollisionCheck(int firstRadius, Coordinate firstCenterPos, 
+								  int secondRadius, Coordinate secondCenterPos)
+{
+	int distance = sqrt(pow(firstCenterPos.posX - secondCenterPos.posX, 2) +
+		pow(firstCenterPos.posY - secondCenterPos.posY, 2));
+
+	if (distance > firstRadius + secondRadius)
+		return false;
+
+	return true;
+}
+
+bool choiceBoxCheckOrCircleCheck(Coordinate boxStartPos, Coordinate boxEndPos, 
+								 Coordinate circleCenterPos)
+{
+	if (circleCenterPos.posX < boxStartPos.posX)
+		return false;
+	if (circleCenterPos.posX > boxEndPos.posX)
+		return false;
+	if (circleCenterPos.posY < boxStartPos.posY)
+		return false;
+	if (circleCenterPos.posY > boxEndPos.posY)
+		return false;
+
+	return true;
 }
